38cfb147d8b90cf11c7aed29ffcc1d13b31e8383
Javier Ramirez <jramir11@bancoestado.cl>
2022-11-10 12:11:11 -0300

    commit inicial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('HelloAngular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Bienvenido a Mean CodingDojo!');
   });
 });
